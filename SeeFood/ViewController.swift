aba7c3f902c2acc1fc11e90c2c5b8b46c9841c28
Pierre-Luc Bruyere <proteama@yahoo.com>
2018-10-24 07:43:31 -0400

    Image analysis for picture

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -29,16 +29,68 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     imagePicker.allowsEditing = false
   }
 
+  // MARK: - CoreML functionality
+
   func imagePickerController(_ picker: UIImagePickerController,
                              didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
   {
     if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
     {
       imageView.image = userPickedImage
+
+      guard let ciimage = CIImage(image: userPickedImage)
+      else
+      {
+        fatalError("Could not convert to CIImage")
+      }
+
+      detect(image: ciimage)
     }
 
     imagePicker.dismiss(animated: true, completion: nil)
   }
+
+  func detect(image: CIImage)
+  {
+    guard let model = try? VNCoreMLModel(for: Inceptionv3().model)
+    else
+    {
+      fatalError("Loading CoreML Model Failed")
+    }
+
+    let request = VNCoreMLRequest(model: model)
+    { (request, error) in
+      guard let results = request.results as? [VNClassificationObservation]
+      else
+      {
+        fatalError("Could not convert results to VNClassificationObservation")
+      }
+
+      if let firstResult = results.first
+      {
+        if firstResult.identifier.contains("hotdog")
+        {
+          self.navigationItem.title = "Hot Dog !!"
+        }
+        else
+        {
+          self.navigationItem.title = firstResult.identifier
+        }
+      }
+    }
+
+    let handler = VNImageRequestHandler(ciImage: image)
+    do
+    {
+      try handler.perform([request])
+    }
+    catch
+    {
+      fatalError("VNImageRequestHandler error")
+    }
+  }
+
+  // MARK: - Navigation bar buttons
 
   @IBAction func cameraTapped(_ sender: UIBarButtonItem)
   {
